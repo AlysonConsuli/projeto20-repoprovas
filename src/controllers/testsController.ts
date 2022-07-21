2dedfea1bd42258b64a9cf368978f4b72fcb5c2d
alyson <consulialyson@gmail.com>
2022-07-21 16:24:18 -0300

    feat: add test

--- src/controllers/testsController.ts
+++ src/controllers/testsController.ts
@@ -4,5 +4,7 @@ import * as testsService from "../services/testsService.js";
 import { TestInsertData } from "../interfaces/createData.js";
 
 export const addTest = async (req: Request, res: Response) => {
+  const test: TestInsertData = req.body;
+  await testsService.addTest(test);
   res.sendStatus(201);
 };
--- src/services/testsService.ts
+++ src/services/testsService.ts
@@ -1,9 +1,17 @@
-import {
-  conflictError,
-  notFoundError,
-  unauthorizedError,
-} from "../middlewares/errorHandlingMiddleware.js";
-import * as testsRepository from "../repositories/testsRepository.js";
+import { notFoundError } from "../middlewares/errorHandlingMiddleware.js";
 import { TestInsertData } from "../interfaces/createData.js";
+import * as testsRepository from "../repositories/testsRepository.js";
 
-export const addTest = async () => {};
+export const addTest = async (test: TestInsertData) => {
+  const category = await testsRepository.findCategoryById(test.categoryId);
+  if (!category) {
+    throw notFoundError("Category not found!");
+  }
+  const teacherDiscipline = await testsRepository.findTeacherDisciplineById(
+    test.teacherDisciplineId
+  );
+  if (!teacherDiscipline) {
+    throw notFoundError("TeacherDiscipline not found!");
+  }
+  await testsRepository.insertTest(test);
+};
