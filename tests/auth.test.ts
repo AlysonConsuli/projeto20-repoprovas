87289da065e27dcf3fae3100a4e46a00433e53c9
alyson <consulialyson@gmail.com>
2022-07-23 21:05:58 -0300

    feat: auth tests

--- tests/auth.test.ts
+++ tests/auth.test.ts
@@ -2,6 +2,7 @@ import supertest from "supertest";
 import app from "../src/app.js";
 import prisma from "../src/config/db.js";
 import * as userFactory from "./factories/userFactory.js";
+import { UserInsertData } from "../src/interfaces/createData.js";
 
 beforeEach(async () => {
   await prisma.$executeRaw`TRUNCATE TABLE users CASCADE`;
@@ -18,6 +19,44 @@ describe("User tests suite", () => {
       where: { email: login.email },
     });
     expect(user.email).toBe(login.email);
+  });
+
+  it("given valid email and password, receive token", async () => {
+    const login = userFactory.createLogin();
+    const user: any = await userFactory.createUser(login);
+
+    const response = await supertest(app).post(`/sign-in`).send({
+      email: user.email,
+      password: user.plainPassword,
+    });
+    const token = response.body.token;
+    expect(token).not.toBeNull();
+  });
+
+  it("given email and password already in use, receive 409", async () => {
+    const login = userFactory.createLogin();
+    await userFactory.createUser(login);
+
+    const response = await supertest(app).post(`/sign-up`).send(login);
+    expect(response.status).toBe(409);
+  });
+
+  it("given invalid password, receive 401", async () => {
+    const login = userFactory.createLogin();
+    await userFactory.createUser(login);
+
+    const response = await supertest(app)
+      .post(`/sign-in`)
+      .send({ ...login, password: "wrong password" });
+    expect(response.status).toBe(401);
+  });
+
+
+  it("given email not registered, receive 404", async () => {
+    const login = userFactory.createLogin();
+
+    const response = await supertest(app).post(`/sign-in`).send(login);
+    expect(response.status).toBe(404);
   });
 });
 
--- tests/factories/userFactory.ts
+++ tests/factories/userFactory.ts
@@ -1,9 +1,23 @@
 import { faker } from "@faker-js/faker";
+import bcrypt from "bcrypt";
+
 import prisma from "../../src/config/db.js";
+import { UserInsertData } from "../../src/interfaces/createData.js";
 
 export const createLogin = (email = "test@test.com", passwordLength = 10) => {
   return {
     email,
     password: faker.internet.password(passwordLength),
   };
+};
+
+export const createUser = async (login: UserInsertData) => {
+  const user = await prisma.users.create({
+    data: {
+      email: login.email,
+      password: bcrypt.hashSync(login.password, 10),
+    },
+  });
+
+  return { ...user, plainPassword: login.password };
 };
