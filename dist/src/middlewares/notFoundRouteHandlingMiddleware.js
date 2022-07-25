04e096c0954f3106c55902b1d0b94f28a0e04dc2
alyson <consulialyson@gmail.com>
2022-07-25 14:46:40 -0300

    fix: duplicate tests

--- dist/src/middlewares/notFoundRouteHandlingMiddleware.js
+++
@@ -1,3 +0,0 @@
-export var notFoundRouteHandlingMiddleware = function (req, res, next) {
-    res.status(404).send("Route not found");
-};
--- dist/src/routes/testsRouter.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { validateSchema } from "../middlewares/schemaValidator.js";
-import { validateToken } from "../middlewares/validateToken.js";
-import { addTest, getTests } from "../controllers/testsController.js";
-import { testSchema } from "../schemas/testsSchema.js";
-var testsRouter = Router();
-testsRouter.post("/test", validateToken, validateSchema(testSchema), addTest);
-testsRouter.get("/tests", validateToken, getTests);
-export default testsRouter;
